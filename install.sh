e21a3c072d2f972ff655170d6e7364851589ba7d
Tomasz Durda <tomasz.durda@sagiton.pl>
2019-09-16 18:25:19 +0200

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+# Install alacritty 
+trizen -Sy --noconfirm \
+   alacritty
+
+# Install rofi
+trizen -Sy --noconfirm \
+   rofi-dmenu
+
+# Install i3
+trizen -Sy --noconfirm \
+   i3-gaps \
+   libevent \
+   dmenu \
+   i3lock
+
+# Install polybar
+trizen -Sy --noconfirm \
+   polybar \
+   termsyn-font \
+   ttf-font-awesome
+
+# Install vim
+trizen -Sy --noconfirm \
+   vim-plug \
+   vim-instant-markdown
+
+# Install zsh
+trizen -Sy --noconfirm \
+   antibody \
+   oh-my-zsh \
+   screenfetchi \
+   neofetch
+
+# Install tmux
+trizen -Sy --noconfirm \
+   hstr \
+   tmux
+
