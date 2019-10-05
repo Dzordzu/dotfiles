67cc4100031603e9b46d3023c5318b278aeced35
Tomasz Durda <tomasz.durda@sagiton.pl>
2019-10-05 14:25:07 +0200

    Changed to more agile approach (user creation)

--- prepare-system.sh
+++ prepare-system.sh
@@ -1,13 +1,10 @@
 
 name=''
-if [ -z "$1" ]; then
-   name="dzordzu"
-else
-   name=$1
+if [ ! -z "$1" ]; then
+   name="$1"
+   useradd -m $1
+   su $1
 fi
-
-useradd -m $1
-su $1
 
 # Install trizen 
 git clone https://aur.archlinux.org/trizen.git
