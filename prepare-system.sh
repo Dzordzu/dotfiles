f16e65d66b0f007255f91a83929306e2505a21c6
Tomasz Durda <tomasz.durda@sagiton.pl>
2019-10-05 18:46:29 +0200

    Upgraded system preparation

--- prepare-system.sh
+++ prepare-system.sh
@@ -6,10 +6,19 @@ git clone https://aur.archlinux.org/trizen.git
 cd trizen
 makepkg -si --noconfirm
 
-# Install base
+# Install basic compression
 trizen -Sy --noconfirm \
    perl-lwp-protocol-https \
-   bluez \
    p7zip \
    gzip \
    tar
+
+trizen -Sy --noconfirm \
+   sddm \
+   xorg
+
+sudo systemctl enable sddm
+sudo systemctl start sddm
+
+trizen -Sy --noconfirm \
+   bluez
