19e4e07dbedb0518158f05e045a67ffe962af2e3
Tomasz Durda <tomasz.durda@sagiton.pl>
2019-10-05 18:32:26 +0200

    Added noconfirm

--- prepare-system.sh
+++ prepare-system.sh
@@ -4,7 +4,7 @@ sudo pacman -Sy --noconfirm base-devel
 # Install trizen 
 git clone https://aur.archlinux.org/trizen.git
 cd trizen
-makepkg -si
+makepkg -si --noconfirm
 
 # Install base
 trizen -Sy --noconfirm \
